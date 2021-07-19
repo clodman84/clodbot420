083c5ffc063ac2f36f5c3ba655a4fecacd1efa2a
clodman84 <77118531+clodman84@users.noreply.github.com>
2021-07-19 10:47:53 +0530

    DM fix

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def on_message(message):
         await channel.send(str(message.content))
 
     if STUDY[1] and message.channel.id != 866030261341650953 and message.author.id in [i.id for i in STUDY[2].keys()]:
-        await bot.send_message(message.author, f'Don\'t stray from the path to **FOREVER MONKE**. Focus yung wan, you can talk when '
+        await message.author.send(f'Don\'t stray from the path to **FOREVER MONKE**. Focus yung wan, you can talk when '
                                    'you have a break.')
         return
     '''
