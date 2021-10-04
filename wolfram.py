03a9bc76da8ae0b6cf57524b4cd28534c24dcca1
clodman84 <77118531+clodman84@users.noreply.github.com>
2021-10-04 23:36:04 +0530

    wolfram

--- wolfram.py
+++ wolfram.py
@@ -5,20 +5,12 @@ import urllib.parse
 
 async def WolframSearch(query):
     query = urllib.parse.quote_plus(query)
-    query_url = f"http://api.wolframalpha.com/v2/query?" \
+    query_url = f"http://api.wolframalpha.com/v2/result?" \
                 f"appid={WOLFRAM_APP_ID}" \
                 f"&input={query}" \
                 f"&format=plaintext" \
-                f"&includepodid=Result"\
-                f"&output=json"
+
     result = await get(query_url)
     if result.status_code != 200:
         return 'Could not retrieve information from Wolfram|Alpha.'
-
-    result = result.json()['queryresult']
-
-    if result['success']:
-        data = result['pods'][0]['subpods'][0]
-        return data['plaintext']
-    else:
-        return 'Does not compute.'
+    return result.text
