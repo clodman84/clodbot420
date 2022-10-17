cb2db2d72c36fb75803de1ad6f011ccacb489872
clodman84 <77118531+clodman84@users.noreply.github.com>
2022-10-17 08:22:21 +0530

    based detection with regex

--- cogs/pills.py
+++ cogs/pills.py
@@ -3,6 +3,7 @@ from cogs.discord_utils.embeds import ClodEmbed
 import discord
 import clodbot.database as database
 import time
+import re
 from clodbot.utils import SimpleTimer
 from bot import ClodBot
 
@@ -10,20 +11,24 @@ from bot import ClodBot
 class PillsCog(commands.Cog):
     def __init__(self, bot: ClodBot):
         self.bot = bot
+        self.basedRegex = re.compile(
+            r"based and (([\w\"'-]+\s+)+)pilled", re.IGNORECASE
+        )
 
     @commands.Cog.listener("on_message")
     async def basedDetector(self, message: discord.Message):
         if message.reference is None:
             return
-        content = message.content.lower().split()
-        if not (content[:2] == ["based", "and"] and content[-1] == "pilled"):
+        match = self.basedRegex.search(message.content)
+        if not match:
             return
         og: discord.Message = message.reference.resolved
         if og.author.id == message.author.id or og.content == "":
             return
+        pillMessage = match.group(1).strip()
         pill = database.Pill(
             int(time.time()),
-            " ".join(content[3:-1]),
+            pillMessage,
             og.content,
             message.author.id,
             og.author.id,
@@ -35,7 +40,7 @@ class PillsCog(commands.Cog):
             await database.insertPill(pill, self.bot.db)
 
         embed = ClodEmbed(
-            description=f"{og.author.mention} your based counter increased by one!"
+            description=f"{og.author.mention} you are now based and {pillMessage} pilled!"
         ).set_footer(text=timer)
         await message.channel.send(embed=embed)
 
